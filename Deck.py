c79cf2a875859e1ff8194da91f66d316ff08b0da
Nick Bridges <nb1115@wildcats.unh.edu>
2021-10-14 15:39:23 -0400

    Added Deck.py

---
+++ Deck.py
@@ -0,0 +1,5 @@
+class Deck:
+    CardsList = ["AS", "4C", "3H"]
+
+    def __init__(self):
+        self.cards = ["AS", "4C", "3H"]
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from Deck import Deck
+
+
 def main():
     print("test")
+    deck = Deck()
+    print(deck.cards[1])
+
+
 
 if __name__ == "__main__":
     main()
